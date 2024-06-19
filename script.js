68da92186dd2ee991512787d0b27e1c8b7e6f120
Lizeth Domínguez Vivas <126223078+LizethDv@users.noreply.github.com>
2024-06-19 11:12:24 -0600

    fadein p

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ document.getElementById('rsvp-form').addEventListener('submit', function(event) 
     const name = document.getElementById('name').value;
 
     // Mensaje de confirmación
-    const message = `¡Gracias, ${name}, por confirmar tu asistencia! Te esperamos`;
+    const message = `¡Gracias, ${name}, por confirmar tu asistencia! Te esperamos✨`;
     document.getElementById('confirmation-message').textContent = message;
 
     // Número de WhatsApp donde se enviará la confirmación
-    const phoneNumber = '5541832833'; // Reemplaza con el número de teléfono real (incluyendo el código de país, pero sin el símbolo +)
+    const phoneNumber = '5613925902';
 
     // Mensaje de WhatsApp
     const whatsappMessage = `Hola, soy ${name}. Confirmo mi asistencia al bautizo de July.`;
 
-    // Construir URL de WhatsApp
+    // URL de WhatsApp
     const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
 
     // Abrir ventana de WhatsApp
     window.open(whatsappURL, '_blank');
 });
 
-// scripts.js
+// animación
 
 document.addEventListener("DOMContentLoaded", function() {
     const img1 = document.querySelector("img");
